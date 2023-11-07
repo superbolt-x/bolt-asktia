1f20447bd6e82cc230aa86be61800cc235627319
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-11-07 16:33:44 +0100

    Update googleads_ad_performance_headline.sql

--- models/reporting/googleads_ad_performance_headline.sql
+++ models/reporting/googleads_ad_performance_headline.sql
@@ -14,7 +14,7 @@ WITH cleaned_ad_data as
             WHEN SPLIT_PART(SPLIT_PART(responsive_search_ad_headlines,'","assetPerformanceLabel"',{{ headline_id }}|int),'{"text":"',2) ~* '\u0026' 
                 THEN REPLACE(SPLIT_PART(SPLIT_PART(responsive_search_ad_headlines,'","assetPerformanceLabel"',{{ headline_id }}|int),'{"text":"',2),'\\u0026',CHR(38))
             ELSE SPLIT_PART(SPLIT_PART(responsive_search_ad_headlines,'","assetPerformanceLabel"',{{ headline_id }}|int),'{"text":"',2)
-        END as {{ adapter.quote('headline_'~{{ headline_id }}) }},
+        END as {{ adapter.quote('headline_'~headline_id) }},
       {% endfor %}
       {% for description_id in description_ids %}
         CASE
@@ -23,7 +23,7 @@ WITH cleaned_ad_data as
             WHEN SPLIT_PART(SPLIT_PART(responsive_search_ad_descriptions,'","assetPerformanceLabel"',{{ description_id }}|int),'{"text":"',2) ~* '\u0026' 
                 THEN REPLACE(SPLIT_PART(SPLIT_PART(responsive_search_ad_descriptions,'","assetPerformanceLabel"',{{ description_id }}|int),'{"text":"',2),'\\u0026',CHR(38))
             ELSE SPLIT_PART(SPLIT_PART(responsive_search_ad_descriptions,'","assetPerformanceLabel"',{{ description_id }}|int),'{"text":"',2)
-        END as {{ adapter.quote('description_'~{{ description_id }}) }},
+        END as {{ adapter.quote('description_'~description_id) }},
       {% endfor %}
         ad_final_urls,
         cost_micros/1000000 as spend,
